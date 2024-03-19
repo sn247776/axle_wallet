b44e99833d2aac284d15a114e6608cc8208bd78a
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2024-03-19 13:22:00 +0530

    profile screen done

---
+++ lib/nav_bar.dart
@@ -0,0 +1,24 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class NabBar extends StatelessWidget {
+  const NabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
+      body: Container(color: Colors.blueAccent),
+    );
+  }
+}
